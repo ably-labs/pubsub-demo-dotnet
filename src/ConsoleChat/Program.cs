d3c1f27aaf262664ff402183deb6385a2b219453
Marc Duiker <marcduiker@gmail.com>
2022-03-10 16:35:39 +0100

    Reorder to show imporant config first

--- src/ConsoleChat/Program.cs
+++ src/ConsoleChat/Program.cs
@@ -1,9 +1,9 @@
 ﻿var app = new CommandApp();
 app.Configure(config =>
 {
-    config.SetApplicationName("ConsoleChat.exe");
     config.AddCommand<PublishCommand>("pub");
     config.AddCommand<SubscribeCommand>("sub");
+    config.SetApplicationName("ConsoleChat.exe");
     config.AddExample(new[] { "pub", "channel1", "AblyApiKey" });
     config.AddExample(new[] { "sub", "channel1", "AblyApiKey" });
 });
